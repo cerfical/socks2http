9cf7aac75632651fcf750f79fc2aed6d0a776542
cerfical <138404639+cerfical@users.noreply.github.com>
2024-11-26 21:57:37 +0500

    Ensure proper closure of HTTP CONNECT tunnels in case of errors

--- cmd/main.go
+++ cmd/main.go
@@ -5,11 +5,12 @@ import (
 	"io"
 	"net"
 	"net/http"
+	"os"
 	"socks2http/internal/args"
 	"socks2http/internal/log"
 	"socks2http/internal/proxy"
-	"socks2http/internal/util"
 	"sync"
+	"time"
 )
 
 func main() {
@@ -52,7 +53,7 @@ func sendRequest(wr http.ResponseWriter, req *http.Request, conn net.Conn) error
 		return err
 	}
 
-	clientConn, err := getRawConnection(wr)
+	clientConn, err := rawConn(wr)
 	if err != nil {
 		return err
 	}
@@ -63,32 +64,61 @@ func sendRequest(wr http.ResponseWriter, req *http.Request, conn net.Conn) error
 }
 
 func setupHTTPTunnel(wr http.ResponseWriter, proxyConn net.Conn) error {
+	defer proxyConn.Close()
+
 	wr.WriteHeader(http.StatusOK)
-	clientConn, err := getRawConnection(wr)
+	clientConn, err := rawConn(wr)
 	if err != nil {
 		return err
 	}
+	defer clientConn.Close()
 
 	wg := sync.WaitGroup{}
-	wg.Add(2)
+	defer wg.Wait()
 
-	transfer := func(dest io.WriteCloser, src io.ReadCloser) {
-		if _, err := io.Copy(dest, src); err != nil {
-			log.Error("HTTP tunnel closed: %v", err)
+	transfer := func(dest, src net.Conn) {
+		defer wg.Done()
+		wg.Add(1)
+
+		reportError := func(conn net.Conn, err error) {
+			// use deadlines to preemptively terminate Read()/Write() calls and avoid goroutines being blocked indefinitely
+			if errors.Is(err, os.ErrDeadlineExceeded) {
+				if err := conn.(*net.TCPConn).SetLinger(0); err != nil {
+					log.Error("failed to reset TCP connection after error: %v", err)
+				}
+			} else {
+				now := time.Now().Add(time.Second * -1)
+				if err := dest.SetReadDeadline(now); err != nil {
+					log.Error("failed to close HTTP tunnel: %v", err)
+				}
+				if err := src.SetWriteDeadline(now); err != nil {
+					log.Error("failed to close HTTP tunnel: %v", err)
+				}
+				log.Error("HTTP tunnel closed abnormally: %v", err)
+			}
 		}
-		wg.Done()
+
+		buf := make([]byte, 1024)
+		for isEof := false; !isEof; {
+			if n, err := src.Read(buf); err != nil {
+				if err != io.EOF {
+					reportError(src, err)
+					break
+				}
+				isEof = true
+			} else if _, err := dest.Write(buf[:n]); err != nil {
+				reportError(dest, err)
+				break
+			}
+		}
 	}
 
 	go transfer(clientConn, proxyConn)
 	transfer(proxyConn, clientConn)
-	wg.Wait()
-
-	util.Must(clientConn.Close())
-	util.Must(proxyConn.Close())
 	return nil
 }
 
-func getRawConnection(wr http.ResponseWriter) (net.Conn, error) {
+func rawConn(wr http.ResponseWriter) (net.Conn, error) {
 	hijacker, ok := wr.(http.Hijacker)
 	if !ok {
 		return nil, errors.New("hijacking not supported")
