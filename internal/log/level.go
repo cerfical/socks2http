7169bdf5b1c79b222e89274c77cbc2de5eaf8361
cerfical <138404639+cerfical@users.noreply.github.com>
2024-12-18 00:47:49 +0500

    Add doc comments to log

--- internal/log/level.go
+++ internal/log/level.go
@@ -2,10 +2,16 @@ package log
 
 import "github.com/rs/zerolog"
 
+// Level defines supported severity levels of log messages.
 type Level zerolog.Level
 
 const (
+	// FatalLevel describe errors that cannot be handled gracefully and typically cause an application to exit.
 	FatalLevel = Level(zerolog.FatalLevel)
+
+	// ErrorLevel describe errors that can either be recovered or safely ignored.
 	ErrorLevel = Level(zerolog.ErrorLevel)
-	InfoLevel  = Level(zerolog.InfoLevel)
+
+	// InfoLevel provides informational messages that may be useful to the end user.
+	InfoLevel = Level(zerolog.InfoLevel)
 )
