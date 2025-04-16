21eb22c5d356a3777c15c159ce97b63fd55ca3ef
cerfical <138404639+cerfical@users.noreply.github.com>
2025-04-16 19:14:16 +0500

    feat: add clean exit

--- cmd/socks2http/main.go
+++ cmd/socks2http/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/cerfical/socks2http/config"
 	"github.com/cerfical/socks2http/log"
@@ -13,9 +15,7 @@ import (
 
 func main() {
 	config := config.Load(os.Args)
-	log := log.New(
-		log.WithLevel(config.LogLevel),
-	)
+	log := log.New(log.WithLevel(config.LogLevel))
 
 	client, err := proxcli.New(
 		proxcli.WithProxyAddr(&config.ProxyAddr),
@@ -24,16 +24,11 @@ func main() {
 		log.Error("Failed to initialize a proxy client", err)
 		return
 	}
-
-	log.Info("Using a proxy",
-		"addr", &config.ProxyAddr,
-	)
+	log.Info("Using a proxy", "addr", &config.ProxyAddr)
 
 	server, err := proxserv.New(
 		proxserv.WithProto(config.ServeAddr.Scheme),
-		proxserv.WithProxy(
-			proxy.New(client),
-		),
+		proxserv.WithProxy(proxy.New(client)),
 		proxserv.WithLog(log),
 	)
 	if err != nil {
@@ -41,8 +36,30 @@ func main() {
 		return
 	}
 
-	if err := server.ListenAndServe(context.Background(), &config.ServeAddr.Host); err != nil {
-		log.Error("Server terminated abnormally", err)
-		return
+	ctx, cancel := context.WithCancel(context.Background())
+	done := make(chan struct{})
+
+	go func() {
+		defer close(done)
+		if err := server.ListenAndServe(ctx, &config.ServeAddr.Host); err != nil {
+			log.Error("Server terminated abnormally", err)
+		}
+	}()
+
+	stop := make(chan os.Signal, 2)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+
+	// Wait for interrupts, and if one occurs, shut down the server
+	<-stop
+	log.Info("Shutting down the server")
+	cancel()
+
+	select {
+	case <-stop:
+		// If another interrupt occurs, abort the shutdown and exit immediately
+		log.Info("Shutdown aborted")
+	case <-done:
+		// The server shutdown was completed
+		log.Info("Server is down")
 	}
 }
