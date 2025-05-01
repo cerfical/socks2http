1992b7e72d6aacb8e1ba64a90448d88ebd422340
cerfical <138404639+cerfical@users.noreply.github.com>
2025-05-01 20:56:37 +0500

    test: improve tests structure

--- config/config_test.go
+++ config/config_test.go
@@ -3,49 +3,62 @@ package config_test
 import (
 	"fmt"
 	"testing"
+	"time"
 
+	"github.com/cerfical/socks2http/addr"
 	"github.com/cerfical/socks2http/config"
-	"github.com/stretchr/testify/assert"
+	"github.com/cerfical/socks2http/log"
+	"github.com/stretchr/testify/suite"
 )
 
-func TestLoad(t *testing.T) {
-	tests := map[string]struct {
+func TestConfig(t *testing.T) {
+	suite.Run(t, new(ConfigTest))
+}
+
+type ConfigTest struct {
+	suite.Suite
+}
+
+func (t *ConfigTest) TestLoad() {
+	flagTests := map[string]struct {
 		arg  string
-		want func(*config.Config) any
+		want func(*config.Config)
 	}{
 		"serve": {
 			arg: "http://localhost:8090",
-			want: func(c *config.Config) any {
-				return c.ServeAddr.String()
+			want: func(c *config.Config) {
+				want := addr.New(addr.HTTP, "localhost", 8090)
+				t.Equal(want, &c.ServeAddr)
 			},
 		},
 
 		"proxy": {
 			arg: "http://localhost:8090",
-			want: func(c *config.Config) any {
-				return c.ProxyAddr.String()
+			want: func(c *config.Config) {
+				want := addr.New(addr.HTTP, "localhost", 8090)
+				t.Equal(want, &c.ProxyAddr)
 			},
 		},
 
 		"timeout": {
 			arg: "12s",
-			want: func(c *config.Config) any {
-				return c.Timeout.String()
+			want: func(c *config.Config) {
+				t.Equal(time.Second*12, c.Timeout)
 			},
 		},
 
 		"log": {
 			arg: "info",
-			want: func(c *config.Config) any {
-				return c.LogLevel.String()
+			want: func(c *config.Config) {
+				t.Equal(log.Info, c.LogLevel)
 			},
 		},
 	}
 
-	for name, test := range tests {
-		t.Run(fmt.Sprintf("supports %s flag", name), func(t *testing.T) {
-			config := config.Load([]string{"", fmt.Sprintf("--%s", name), test.arg})
-			assert.Equal(t, test.arg, test.want(config))
+	for flagName, test := range flagTests {
+		t.Run(fmt.Sprintf("supports %s flag", flagName), func() {
+			config := config.Load([]string{"", fmt.Sprintf("--%s", flagName), test.arg})
+			test.want(config)
 		})
 	}
 }
