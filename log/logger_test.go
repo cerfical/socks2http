47cd7a1f135b60be35ac5947f5789262b8b4da95
cerfical <138404639+cerfical@users.noreply.github.com>
2025-05-05 15:10:02 +0500

    test: clean up logger tests

--- log/logger_test.go
+++ log/logger_test.go
@@ -8,36 +8,44 @@ import (
 	"testing"
 
 	"github.com/cerfical/socks2http/log"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
 func TestLogger(t *testing.T) {
+	suite.Run(t, new(LoggerTest))
+}
+
+type LoggerTest struct {
+	suite.Suite
+}
+
+func (t *LoggerTest) TestLog() {
 	levels := []log.Level{
 		log.LevelInfo,
 		log.LevelError,
 	}
 
 	for _, level := range levels {
-		t.Run(fmt.Sprintf("%[1]v is logged if level is %[1]v or higher", level), func(t *testing.T) {
-			got := writeLog(level, level)
+		t.Run(fmt.Sprintf("%[1]v is logged if log level is %[1]v or higher", level), func() {
+			got := encodeLog(level, level)
 
-			assert.Contains(t, got, "log message")
-			assert.Regexp(t, regexp.MustCompile("error(.*)description"), got)
+			t.Contains(got, "log message")
+			t.Regexp(regexp.MustCompile("error(.*)description"), got)
 		})
 
-		t.Run(fmt.Sprintf("%[1]v is not logged if level is lower", level), func(t *testing.T) {
-			got := writeLog(level-1, level)
-			assert.Equal(t, "", got)
+		t.Run(fmt.Sprintf("%v is not logged if log level is lower", level), func() {
+			got := encodeLog(level-1, level)
+			t.Equal("", got)
 		})
 	}
 
-	t.Run("silent is never logged", func(t *testing.T) {
-		got := writeLog(log.LevelSilent, log.LevelInfo)
-		assert.Equal(t, "", got)
+	t.Run("silent is never logged", func() {
+		got := encodeLog(log.LevelSilent, log.LevelInfo)
+		t.Equal("", got)
 	})
 }
 
-func writeLog(logLevel, msgLevel log.Level) string {
+func encodeLog(logLevel, msgLevel log.Level) string {
 	var buf bytes.Buffer
 	l := log.New(log.WithLevel(logLevel), log.WithWriter(&buf))
 
