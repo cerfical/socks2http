6a2ee474aa8f37cce2692423e170673dd4557b74
cerfical <138404639+cerfical@users.noreply.github.com>
2025-06-04 23:36:48 +0500

    feat(client): add basic host-based proxy routing

---
+++ internal/proxy/router/router_test.go
@@ -0,0 +1,85 @@
+package router_test
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/cerfical/socks2http/internal/proxy"
+	"github.com/cerfical/socks2http/internal/proxy/addr"
+	"github.com/cerfical/socks2http/internal/proxy/router"
+	"github.com/cerfical/socks2http/internal/test/mocks"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
+)
+
+func TestRouter(t *testing.T) {
+	suite.Run(t, new(RouterTest))
+}
+
+type RouterTest struct {
+	suite.Suite
+}
+
+func (t *RouterTest) TestDial() {
+	t.Run("routes connection requests to the destination through specified proxy", func() {
+		dstAddr1 := addr.New("dst-addr-1", 80)
+		proxyAddr1 := addr.New("proxy-1", 8080)
+
+		dstAddr2 := addr.New("dst-addr-2", 80)
+		proxyAddr2 := addr.New("proxy-2", 8080)
+
+		dialer := mocks.NewDialer(t.T())
+		dialer.EXPECT().
+			Dial(mock.Anything, proxyAddr1).
+			Return(nil, errors.New("redirected to Proxy#1"))
+		dialer.EXPECT().
+			Dial(mock.Anything, proxyAddr2).
+			Return(nil, errors.New("redirected to Proxy#2"))
+
+		router := router.New(router.WithDialer(dialer)).
+			Route(dstAddr1.Host, &router.Policy{
+				Proxy: router.ProxyConfig{
+					Addr:  *proxyAddr1,
+					Proto: proxy.ProtoHTTP,
+				},
+			}).
+			Route(dstAddr2.Host, &router.Policy{
+				Proxy: router.ProxyConfig{
+					Addr:  *proxyAddr2,
+					Proto: proxy.ProtoHTTP,
+				},
+			})
+
+		// Check that the first proxy is called for the first address
+		_, err := router.Dial(context.Background(), dstAddr1)
+		t.ErrorContains(err, "Proxy#1")
+
+		// Check that the second proxy is called for the second address
+		_, err = router.Dial(context.Background(), dstAddr2)
+		t.ErrorContains(err, "Proxy#2")
+	})
+
+	t.Run("uses default policy if routing table contains no matches", func() {
+		dstAddr := addr.New("example.com", 80)
+		proxyAddr := addr.New("proxy", 8081)
+
+		dialer := mocks.NewDialer(t.T())
+		dialer.EXPECT().
+			Dial(mock.Anything, proxyAddr).
+			Return(nil, errors.New("redirected to Proxy"))
+
+		router := router.New(
+			router.WithDialer(dialer),
+			router.WithDefaultPolicy(&router.Policy{
+				Proxy: router.ProxyConfig{
+					Addr:  *proxyAddr,
+					Proto: proxy.ProtoHTTP,
+				},
+			}),
+		)
+
+		_, err := router.Dial(context.Background(), dstAddr)
+		t.ErrorContains(err, "Proxy")
+	})
+}
