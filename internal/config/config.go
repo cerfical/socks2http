8713b9c3e93541e95fbdd6b4d8fa768bf9ceedc4
cerfical <138404639+cerfical@users.noreply.github.com>
2025-06-06 03:31:58 +0500

    feat(config): add `--config-file` flag

--- internal/config/config.go
+++ internal/config/config.go
@@ -11,24 +11,27 @@ import (
 	"github.com/cerfical/socks2http/internal/log"
 	"github.com/cerfical/socks2http/internal/proxy"
 	"github.com/cerfical/socks2http/internal/proxy/addr"
+	"github.com/mitchellh/mapstructure"
 	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
 )
 
-func Load(args []string) *Config {
-	config := Config{
-		Proxy: ProxyConfig{
-			Proto: proxy.ProtoDirect,
-		},
-		Server: ServerConfig{
-			Proto: proxy.ProtoHTTP,
-			Addr:  *addr.New("localhost", 8080),
-		},
-		Log: LogConfig{
-			Level: log.LevelVerbose,
-		},
-	}
+var defaultConfig = Config{
+	Proxy: ProxyConfig{
+		Proto: proxy.ProtoDirect,
+	},
+	Server: ServerConfig{
+		Proto: proxy.ProtoHTTP,
+		Addr:  *addr.New("localhost", 8080),
+	},
+	Log: LogConfig{
+		Level: log.LevelVerbose,
+	},
+}
 
+func Load(args []string) *Config {
 	progName := getProgramName(args)
+
 	flags := pflag.NewFlagSet(progName, pflag.ContinueOnError)
 	flags.Usage = func() {
 		fmt.Printf("Usage:\n")
@@ -36,23 +39,79 @@ func Load(args []string) *Config {
 		fmt.Printf("Options:\n")
 		flags.PrintDefaults()
 	}
+	if err := parseFlags(flags, args); err != nil {
+		printErrorAndExit(flags, err)
+	}
 
-	flags.Var(&textValue{&config.Server.Addr}, "server-addr", "address for proxy server to listen on")
-	flags.Var(&textValue{&config.Proxy.Addr}, "proxy-addr", "proxy address to connect via proxy client")
-	flags.Var(&textValue{&config.Server.Proto}, "server-proto", "proxy server protocol to use")
-	flags.Var(&textValue{&config.Proxy.Proto}, "proxy-proto", "proxy client protocol to use")
-	flags.Var(&textValue{&config.Log.Level}, "log-level", "severity level of logging messages")
-	flags.DurationVar(&config.Timeout, "timeout", 0, "``wait duration for I/O operations")
+	config, err := parseConfig(flags)
+	if err != nil {
+		printErrorAndExit(flags, err)
+	}
+	return config
+}
 
-	help := flags.Bool("help", false, "``display help message")
-	if err := flags.Parse(args[1:]); err != nil || *help {
-		if err != nil {
-			fmt.Printf("Error: %v\n", err)
+func printErrorAndExit(f *pflag.FlagSet, err error) {
+	fmt.Printf("Error: %v\n", err)
+	f.Usage()
+	os.Exit(1)
+}
+
+func parseConfig(f *pflag.FlagSet) (*Config, error) {
+	v := viper.New()
+
+	// Bind command-line flags to their corresponding values from config file
+	configNames := []string{"server.addr", "server.proto", "proxy.addr", "proxy.proto", "log.level", "timeout"}
+	for _, name := range configNames {
+		kebabCasedName := strings.ReplaceAll(name, ".", "-")
+		if err := v.BindPFlag(name, f.Lookup(kebabCasedName)); err != nil {
+			panic(fmt.Errorf("bind flag: %w", err))
 		}
-		flags.Usage()
+	}
+
+	v.SetConfigFile(f.Lookup("config-file").Value.String())
+	if err := v.ReadInConfig(); err != nil {
+		// Make the configuration file optional
+		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+			return nil, fmt.Errorf("load config file: %w", err)
+		}
+	}
+
+	options := []viper.DecoderConfigOption{
+		viper.DecodeHook(mapstructure.ComposeDecodeHookFunc(
+			mapstructure.TextUnmarshallerHookFunc(),
+			mapstructure.StringToTimeDurationHookFunc(),
+		)),
+	}
+
+	config := defaultConfig
+	if err := v.UnmarshalExact(&config, options...); err != nil {
+		return nil, fmt.Errorf("parse config file: %w", err)
+	}
+	return &config, nil
+}
+
+func parseFlags(f *pflag.FlagSet, args []string) error {
+	// Flags shared with options from a configuration file
+	c := defaultConfig
+	f.Var(&textValue{&c.Server.Addr}, "server-addr", "address for proxy server to listen on")
+	f.Var(&textValue{&c.Proxy.Addr}, "proxy-addr", "proxy address to connect via proxy client")
+	f.Var(&textValue{&c.Server.Proto}, "server-proto", "proxy server protocol to use")
+	f.Var(&textValue{&c.Proxy.Proto}, "proxy-proto", "proxy client protocol to use")
+	f.Var(&textValue{&c.Log.Level}, "log-level", "severity level of logging messages")
+	f.DurationVar(&c.Timeout, "timeout", c.Timeout, "``wait duration for I/O operations")
+
+	help := f.Bool("help", false, "``display help message")
+	f.String("config-file", "", "``configuration file")
+
+	if err := f.Parse(args[1:]); err != nil {
+		return fmt.Errorf("parse flags: %w", err)
+	}
+
+	if *help {
+		f.Usage()
 		os.Exit(2)
 	}
-	return &config
+	return nil
 }
 
 func getProgramName(args []string) string {
